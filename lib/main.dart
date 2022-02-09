2b3468759d8d145c377054910e09006517e77da3
Uzair Ahmad <42337244+uzair657@users.noreply.github.com>
2022-02-09 17:57:45 +0500

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text("I Am Rich"),
+        backgroundColor: Colors.blueGrey[900],
+        foregroundColor: Colors.white,
+      ),
+      body: Center(
+        child: Image.asset(
+          "images/diamond.png",
+          height: 200.0,
+          width: 200.0,
+        ),
+      ),
+    ),
+  ));
+}
