dcc48fa816505ffb4b1c8e9f65182772ce7647ba
Uzair Ahmad <42337244+uzair657@users.noreply.github.com>
2022-02-10 13:09:06 +0500

    App Icons Update for both IOS and Android.

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,13 @@ void main() {
     home: Scaffold(
       backgroundColor: Colors.blueGrey,
       appBar: AppBar(
-        title: Text("I Am Rich"),
+        title: Text('I Am Rich'),
         backgroundColor: Colors.blueGrey[900],
         foregroundColor: Colors.white,
       ),
       body: Center(
         child: Image.asset(
-          "images/diamond.png",
+          'images/diamond.png',
           height: 200.0,
           width: 200.0,
         ),
